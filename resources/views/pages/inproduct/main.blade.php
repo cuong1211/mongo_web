717147ac0a1e0794999142af4dc38bfa048c26a5
cuong1211 <cuonghaha43@gmail.com>
2022-11-17 12:37:52 +0700

    add 92,5%

--- resources/views/pages/inproduct/main.blade.php
+++ resources/views/pages/inproduct/main.blade.php
@@ -1,3 +1,32 @@
+@extends('layout.source')
+@section('title')
+Purchase
+@endsection
+@extends('layout.header')
+@section('breadcrumb')
+<div class="page-title d-flex flex-column me-5">
+    <!--begin::Title-->
+    <h1 class="d-flex flex-column text-dark fw-bolder fs-3 mb-0">Purchase</h1>
+    <!--end::Title-->
+    <!--begin::Breadcrumb-->
+    <ul class="breadcrumb breadcrumb-separatorless fw-bold fs-7 pt-1">
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-muted">
+            <a href="{{route('admin')}}" class="text-muted text-hover-primary">Home</a>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item">
+            <span class="bullet bg-gray-200 w-5px h-2px"></span>
+        </li>
+        <!--end::Item-->
+        <!--begin::Item-->
+        <li class="breadcrumb-item text-dark">Purchase</li>
+        <!--end::Item-->
+    </ul>
+    <!--end::Breadcrumb-->
+</div>
+@endsection
 @extends('layout.index')
 @section('content')
     <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
